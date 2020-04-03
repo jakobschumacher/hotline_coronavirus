105dce3de57912ffb912dadc884bf91086a70d02
Jakob Schumacher <jakob.schumacher@web.de>
2020-04-03 19:43:03 +0200

    Changes before Ubuntu update

--- _posts/2020-03-21-abstrichmanagement.markdown
+++ _posts/2020-03-21-abstrichmanagement.markdown
@@ -7,4 +7,36 @@ img: swab.jpg # Add image post (optional)
 fig-caption: # Add figcaption (optional)
 tags: [Eingabe]
 ---
-
+<div class="mermaid">
+graph TD
+     1[Erfassung des Sachverhalts]
+     2a[Covid-Fall]
+     2b[Zuständigkeit Gesi<br />Weitergabe an 5030]
+     3a[Medizinisches Personal]
+     3b[Zuständigkeit Gesi<br />Weitergabe an 5030]
+     4a[Befund Auskunft]
+     4b[Zuständigkeit Fr. Sackschweski<br />Weitergabe an 5069]
+     5a[Anfrage nicht zu <br /> Coronavirus]
+     5b[Zuständigkeit GesSekr<br />Weitergabe an 5079]
+     6a[Anfrage zu <br />Unterbringung<br />Nahrungsmittel]
+     6b[Zuständigkeit Dudey<br /> Weitergabe an ]
+     7a[Anfrage zu Ehrenamtlicher Tätigkeit]
+     7b[Zuständigkeit <br />Ehrenamtsmangement <br /> Weitergabe an ]
+     8a[Eigene Zuständigkeit]
+     
+     
+     1--&gt;2a
+     2a--Ja--&gt;2b
+     2a--Nein--&gt;3a
+     3a--Ja--&gt;3b
+     3a--Nein--&gt;4a
+     4a--Ja--&gt;4b
+     4a--Nein--&gt;5a
+     5a--Ja--&gt;5b
+     5a--Nein--&gt;6a
+     6a--Ja--&gt;6b
+     6a--Nein--&gt;7a
+     7a--Ja--&gt;7b
+     7a--Nein--&gt;8a
+     
+    </div>
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,54 +2,36 @@ var store = [{
         "title": "Überblick über die Bereiche",
         "excerpt":"Leitung Gesundheitsamt (90294 - ) Leitung und Organisation Gesundheitsamt Weiterentwicklung Lagezentrum Leitung und Organisation Lagezentrum Sekretariat Gesundheitsamt (Telefonnummer: 90294 - ) Abarbeitung der E-Mailadresse gesundheitsamt@reinickendorf.berlin.de. Bei positivem Laborbefund telefonische Information von Hr. XXX Telefonnummer 90294-XXXX Laborbefunde an laborbefunde@reinckendorf.berlin.de Chefanfrage an XXX und XXX (in CC) Coronavirus an coronavirus@reinickendorf.berlin.de Sonstige Emails...","categories": [],
         "tags": ["Überblick"],
-        "url": "http://localhost:4000/lagezentrum/bereiche/",
+        "url": "http://localhost:4000/bereiche/",
         "teaser": null
       },{
         "title": "Interne Fragen und Antworten",
         "excerpt":" ","categories": [],
         "tags": ["Eingabe"],
-        "url": "http://localhost:4000/lagezentrum/faq/",
+        "url": "http://localhost:4000/faq/",
         "teaser": null
       },{
         "title": "Laborbefunde",
         "excerpt":" ","categories": [],
         "tags": ["Eingabe"],
-        "url": "http://localhost:4000/lagezentrum/lab/",
+        "url": "http://localhost:4000/lab/",
         "teaser": null
       },{
         "title": "Abstrichmanagement",
-        "excerpt":" ","categories": [],
+        "excerpt":"graph TD 1[Erfassung des Sachverhalts] 2a[Covid-Fall] 2b[Zuständigkeit Gesi Weitergabe an 5030] 3a[Medizinisches Personal] 3b[Zuständigkeit Gesi Weitergabe an 5030] 4a[Befund Auskunft] 4b[Zuständigkeit Fr. Sackschweski Weitergabe an 5069] 5a[Anfrage nicht zu Coronavirus] 5b[Zuständigkeit GesSekr Weitergabe an 5079] 6a[Anfrage zu Unterbringung Nahrungsmittel] 6b[Zuständigkeit Dudey Weitergabe an ] 7a[Anfrage zu Ehrenamtlicher Tätigkeit] 7b[Zuständigkeit Ehrenamtsmangement...","categories": [],
         "tags": ["Eingabe"],
-        "url": "http://localhost:4000/lagezentrum/abstrichmanagement/",
-        "teaser": null
-      },{
-        "title": "Klärung der Zuständigkeit",
-        "excerpt":"Wer ist für welchen Sachverhalt zuständig? graph TD 1[Erfassung des Sachverhalts] 2a[Covid-Fall] 2b[Zuständigkeit Gesi Weitergabe an 5030] 3a[Medizinisches Personal] 3b[Zuständigkeit Gesi Weitergabe an 5030] 4a[Befund Auskunft] 4b[Zuständigkeit Fr. Sackschweski Weitergabe an 5069] 5a[Anfrage nicht zu Coronavirus] 5b[Zuständigkeit GesSekr Weitergabe an 5079] 6a[Anfrage zu Unterbringung Nahrungsmittel] 6b[Zuständigkeit Dudey Weitergabe an ]...","categories": [],
-        "tags": ["Hotline","Schulung"],
-        "url": "http://localhost:4000/lagezentrum/hotline-ablauf/",
-        "teaser": null
-      },{
-        "title": "Varianten für CGA App",
-        "excerpt":"Variante A Browser basierte Variante. Die Gesundheitsämter loggen sich ein. Es gibt ein Suchfeld. In das Suchfeld gibt am die TAN des Covid-Falls an. Es erscheinen die Kontakte (UserID oder Alternativ: Name, Telefonnummer, Kontaktwert = Berechnung der Software). Diese Informationen lassen sich nach Excel, SurvNet, Sormas exportieren. Variante B Browser...","categories": [],
-        "tags": ["Eingabe"],
-        "url": "http://localhost:4000/lagezentrum/varianten-fuer-software/",
-        "teaser": null
-      },{
-        "title": "Sequence (draft)",
-        "excerpt":"Base variant Neither labs nor health authority participates graph TD 1[Lab identifies covid-patient] 2[Fax to Health authority] 3[Covid-patient gets notified by lab] 4[Software-info to physician] 1--&gt;2 1--&gt;3 1--&gt;4 5[Health authority contacts covid-patient] 6[Covid-patient enters I have covid] 7[Physicians informs patient] 2--&gt;5 3--&gt;6 4--&gt;7 7--&gt;6 6--&gt;8A[Contact A] 6--&gt;8B[Contact B] 6--&gt;8C[Contact C]...","categories": [],
-        "tags": ["Eingabe"],
-        "url": "http://localhost:4000/lagezentrum/user-journey/",
+        "url": "http://localhost:4000/abstrichmanagement/",
         "teaser": null
       },{
         "title": "Hotline Datenaufnahme",
         "excerpt":"    ","categories": [],
         "tags": ["Eingabe"],
-        "url": "http://localhost:4000/lagezentrum/hotline/",
+        "url": "http://localhost:4000/hotline/",
         "teaser": null
       },{
         "title": "Begrüßung und Schulung",
         "excerpt":" ","categories": [],
         "tags": ["Überblick"],
-        "url": "http://localhost:4000/lagezentrum/infektionsepidemiologie/",
+        "url": "http://localhost:4000/infektionsepidemiologie/",
         "teaser": null
       }]
