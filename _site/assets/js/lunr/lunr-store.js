6b0810d7b6a4e933380209b36bfbd8a664d3e854
Jakob <jakob.schumacher@web.de>
2020-10-12 20:46:31 +0200

    Veränderungen

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,36 +2,42 @@ var store = [{
         "title": "Überblick über die Bereiche",
         "excerpt":"Leitung Gesundheitsamt (90294 - ) Leitung und Organisation Gesundheitsamt Weiterentwicklung Lagezentrum Leitung und Organisation Lagezentrum Sekretariat Gesundheitsamt (Telefonnummer: 90294 - ) Abarbeitung der E-Mailadresse gesundheitsamt@reinickendorf.berlin.de. Bei positivem Laborbefund telefonische Information von Hr. XXX Telefonnummer 90294-XXXX Laborbefunde an laborbefunde@reinckendorf.berlin.de Chefanfrage an XXX und XXX (in CC) Coronavirus an coronavirus@reinickendorf.berlin.de Sonstige Emails...","categories": [],
         "tags": ["Überblick"],
-        "url": "http://localhost:4000/bereiche/",
+        "url": "http://localhost:4000/lagezentrum-coronavirus/bereiche/",
         "teaser": null
       },{
         "title": "Interne Fragen und Antworten",
         "excerpt":" ","categories": [],
         "tags": ["Eingabe"],
-        "url": "http://localhost:4000/faq/",
+        "url": "http://localhost:4000/lagezentrum-coronavirus/faq/",
         "teaser": null
       },{
         "title": "Laborbefunde",
         "excerpt":" ","categories": [],
         "tags": ["Eingabe"],
-        "url": "http://localhost:4000/lab/",
+        "url": "http://localhost:4000/lagezentrum-coronavirus/lab/",
         "teaser": null
       },{
         "title": "Abstrichmanagement",
         "excerpt":"graph TD 1[Erfassung des Sachverhalts] 2a[Covid-Fall] 2b[Zuständigkeit Gesi Weitergabe an 5030] 3a[Medizinisches Personal] 3b[Zuständigkeit Gesi Weitergabe an 5030] 4a[Befund Auskunft] 4b[Zuständigkeit Fr. Sackschweski Weitergabe an 5069] 5a[Anfrage nicht zu Coronavirus] 5b[Zuständigkeit GesSekr Weitergabe an 5079] 6a[Anfrage zu Unterbringung Nahrungsmittel] 6b[Zuständigkeit Dudey Weitergabe an ] 7a[Anfrage zu Ehrenamtlicher Tätigkeit] 7b[Zuständigkeit Ehrenamtsmangement...","categories": [],
         "tags": ["Eingabe"],
-        "url": "http://localhost:4000/abstrichmanagement/",
+        "url": "http://localhost:4000/lagezentrum-coronavirus/abstrichmanagement/",
+        "teaser": null
+      },{
+        "title": "Einleitung",
+        "excerpt":"Für wen ist diese Seite?  Diese Seite ist für Personen, die SARS-Cov-2 Kontaktpersonennachverfolgung im Gesundheitsamt machen. Das beste wäre eine vollständige Ausbildung zum Hygienekontrolleur, z.B. an der Akademie für öffentliches Gesundheitswesen. Diese Ausbildung dauert leider  ","categories": [],
+        "tags": ["Eingabe"],
+        "url": "http://localhost:4000/lagezentrum-coronavirus/Einleitung/",
         "teaser": null
       },{
         "title": "Hotline Datenaufnahme",
         "excerpt":"    ","categories": [],
         "tags": ["Eingabe"],
-        "url": "http://localhost:4000/hotline/",
+        "url": "http://localhost:4000/lagezentrum-coronavirus/hotline/",
         "teaser": null
       },{
         "title": "Begrüßung und Schulung",
         "excerpt":" ","categories": [],
         "tags": ["Überblick"],
-        "url": "http://localhost:4000/infektionsepidemiologie/",
+        "url": "http://localhost:4000/lagezentrum-coronavirus/infektionsepidemiologie/",
         "teaser": null
       }]
